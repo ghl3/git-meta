660150e92e96dd3f98ba75eeab84fe06bd81cdaa
George Lewis <ghl227@gmail.com>
2015-02-05 20:51:19 -0500

    Creating initial functionality

--- git_meta.py
+++ git_meta.py
@@ -1,16 +1,50 @@
 
-import argparse
+from __future__ import print_function
 
+import os
 import sys
 
+import argparse
+
+
+def is_git_init():
+    return os.path.isdir(".git")
+
+
+def has_meta_directory():
+    return os.path.isdir(".git/meta")
+
+
+def create_meta_directory():
+    try:
+        os.makedirs('.git/meta')
+    except OSError:
+        pass
+
+
+def get_current_git_branch():
+    return 'branch'
+
+
+def get_meta_path():
+    branch = get_current_git_branch()
+    return '/.git/meta/{}'.format(branch)
+
+
+def show_metadata():
+    # TODO: Check for existence
+    return open(get_meta_path()).readlines()
 
 
 def set_meta(metadata):
-    pass
+    create_meta_directory()
+    with open(get_meta_path(), 'w+') as f:
+        f.write(metadate)
 
 
 def clear_meta():
-    pass
+    if not has_meta_directory():
+        return
 
 
 def main():
@@ -25,8 +59,11 @@ def main():
 
     args = parser.parse_args()
 
+    if not is_git_init():
+        sys.exit("Not a git directory")
+
     if args.m and parser.clear:
-        sys.exit("Must select either -m or -c options")
+        sys.exit("Must select either -m or -c options, not both")
 
     elif args.m:
         set_meta(args.m)
@@ -35,8 +72,7 @@ def main():
         clear_meta()
 
     else:
-        sys.exit("Must select either -m or -c options")
-
+        show_metadata()
 
 
 if __name__ == '__main__':
