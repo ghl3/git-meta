7c74200df022d90c9529ba76079863e00f521f10
George Lewis <ghl227@gmail.com>
2015-02-05 21:00:42 -0500

    Initial prototype works

--- git_meta.py
+++ git_meta.py
@@ -28,45 +28,47 @@ def get_current_git_branch():
 
 def get_meta_path():
     branch = get_current_git_branch()
-    return '/.git/meta/{}'.format(branch)
+    return '.git/meta/{}'.format(branch)
 
 
 def show_metadata():
-    # TODO: Check for existence
-    return open(get_meta_path()).readlines()
+    try:
+        print("\n".join(open(get_meta_path()).readlines()))
+    except IOError as e:
+        print("No metadata in current branch")
 
 
 def set_meta(metadata):
     create_meta_directory()
-    with open(get_meta_path(), 'w+') as f:
-        f.write(metadate)
+    with open(get_meta_path(), 'w') as f:
+        f.write(metadata)
 
 
 def clear_meta():
     if not has_meta_directory():
         return
-
+    else:
+        os.remove(get_meta_path())
+    
 
 def main():
 
     parser = argparse.ArgumentParser(description='Process some integers.')
 
-    parser.add_argument('-m', type=int,
-                        help='Metadata for the current branch')
+    parser.add_argument('-m', '--meta', help='Metadata for the current branch')
 
-    parser.add_argument('-c', '--clear',
-                        help="Clear the current branch's metadata")
+    parser.add_argument('-c', '--clear', action='store_true', help="Clear the current branch's metadata")
 
     args = parser.parse_args()
 
     if not is_git_init():
         sys.exit("Not a git directory")
 
-    if args.m and parser.clear:
+    if args.meta and args.clear:
         sys.exit("Must select either -m or -c options, not both")
 
-    elif args.m:
-        set_meta(args.m)
+    elif args.meta:
+        set_meta(args.meta)
 
     elif args.clear:
         clear_meta()
